101657532fc4b1a9c76a0ad4deafb8726fac1c5f
chokrihamza <chokrihamza1994@gmail.com>
2021-06-10 08:42:53 +0200

    create db using myslqi oop

--- mysqli_oop.php
+++ mysqli_oop.php
@@ -1,1 +1,25 @@
 <?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// connect to the data base 
+
+$connect = new mysqli($servername, $username, $password);
+
+if ($connect->connect_error) {
+  die("there is a problem when trying connect to DB" . $connect->connect_error);
+}
+
+echo "connected ...";
+// Create data base 
+$sql = "CREATE DATABASE connect";
+
+if ($connect->query($sql) === true) {
+  echo "databae created successfully";
+} else {
+  echo "Error creating DB" . $connect->error;
+}
+
+$connect->close();
