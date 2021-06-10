9493e92b9e1c565158779639c87c9c8cc7a2aacb
chokrihamza <chokrihamza1994@gmail.com>
2021-06-10 10:32:59 +0200

    create atble using pdo and proc

--- mysqli_proc.php
+++ mysqli_proc.php
@@ -29,7 +29,20 @@ if (mysqli_query($connect, $sql)) {
 //   echo "Erro drop db" . mysqli_error($connect);
 // }
 
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
 
+if (mysqli_query($conn, $sql)) {
+  echo "Table MyGuests created successfully";
+} else {
+  echo "Error creating table: " . mysqli_error($conn);
+}
 
 
 mysqli_close($connect);
